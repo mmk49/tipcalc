7e64107f90de27d728abed7fde64141bdd2bdc9c
Mustafa <mustafamkhan28@gmail.com>
2024-10-21 17:00:50 -0700

    added UI + btn toggle functionality

--- app/src/main/java/edu/uw/ischool/mmk49/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/mmk49/tipcalculator/MainActivity.kt
@@ -1,10 +1,14 @@
 package edu.uw.ischool.mmk49.tipcalculator
 
 import android.os.Bundle
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val txt = findViewById<EditText>(R.id.editTextNumberDecimal)
+        val btn = findViewById<Button>(R.id.button)
+        println(btn.isClickable)
+        txt.doOnTextChanged { text, start, before, count ->
+            run {
+                if(text.isNullOrEmpty()) {
+                    btn.isClickable = false
+                    println(btn.isClickable)
+                } else {
+                    btn.isClickable = true
+                    println(btn.isClickable)
+                }
+            }
         }
     }
 }
